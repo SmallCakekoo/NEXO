798e6089b08f8a6fd0e279c54674050dda175e79
Pablo T. <ptinocomv@gmail.com>
2025-05-30 14:33:01 -0500

    fix(post): se cambió el mensaje a comment

--- src/components/feed-components/FeedPost.ts
+++ src/components/feed-components/FeedPost.ts
@@ -539,7 +539,7 @@ class FeedPost extends HTMLElement {
                 <svg class="comment-icon" viewBox="0 0 24 24" fill="currentColor" width="20" height="20">
                   <path d="M21.99 4c0-1.1-.89-2-1.99-2H4c-1.1 0-2 .9-2 2v12c0 1.1.9 2 2 2h14l4 4-.01-18zM18 14H6v-2h12v2zm0-3H6V9h12v2zm0-3H6V6h12v2z"/>
                 </svg>
-                <p class="comments-count">${this.post.comments && this.post.comments.length > 0 ? this.post.comments.length + ' Comments' : 'Be the first to share something interesting'}</p>
+                <p class="comments-count">${this.post.comments && this.post.comments.length > 0 ? this.post.comments.length + ' Comments' : ' comment'}</p>
               </button>
             </div>
 
